b2efad22a8a1d60c1bd4ea32e4a16123fb5be40b
saucymouse <will.staton@gmail.com>
2021-08-09 21:43:57 -0400

    initial commit and working hello world

---
+++ src/test/java/com/fizzybubbly/fizzybubbly/FizzybubblyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fizzybubbly.fizzybubbly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FizzybubblyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
