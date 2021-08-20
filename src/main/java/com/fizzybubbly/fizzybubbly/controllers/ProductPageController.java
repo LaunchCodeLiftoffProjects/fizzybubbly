43636bd827545c008f3f507688f31c08a1739148
zabalav <vincent_zabala@yahoo.com>
2021-08-20 15:34:55 -0500

    Here's my first try at the product page. Not sure if I'm on the right track, but I still haven't figured out how to display info for just one seltzer. Any ideas?

---
+++ src/main/java/com/fizzybubbly/fizzybubbly/controllers/ProductPageController.java
@@ -0,0 +1,38 @@
+package com.fizzybubbly.fizzybubbly.controllers;
+
+import com.fizzybubbly.fizzybubbly.models.Drink;
+import com.fizzybubbly.fizzybubbly.models.DrinkData;
+import com.fizzybubbly.fizzybubbly.models.data.DrinkRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping ("productPage")
+public class ProductPageController {
+
+    @Autowired
+    private DrinkRepository drinkRepository;
+
+    @RequestMapping("")
+    public String productInfo(Model model) {
+        model.addAttribute("title", "seltz info");
+        model.addAttribute("drinks", drinkRepository.findAll());
+        return "productPage";
+    }
+
+    @PostMapping("productPage/drink.id=")
+    public String displaySeltzInfo(Model model, @RequestParam String searchTerm) {
+        Iterable<Drink> drinks = null;
+        if(searchTerm.toLowerCase().equals("")) {
+            drinks = DrinkData.findByValue(searchTerm, drinkRepository.findAll());
+        }
+        model.addAttribute("title", searchTerm + "seltz info");
+        model.addAttribute("drinks", drinks);
+
+        return "productPage";
+    }
+}
