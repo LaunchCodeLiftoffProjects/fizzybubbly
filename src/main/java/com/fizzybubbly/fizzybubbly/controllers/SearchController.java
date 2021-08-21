8a9f3ffe494021607848f8ed8afbef530485b040
James Margherio <jimargherio@me.com>
2021-08-21 17:54:54 -0500

    Search page improved again (sorting results), new .csv with more info

--- src/main/java/com/fizzybubbly/fizzybubbly/controllers/SearchController.java
+++ src/main/java/com/fizzybubbly/fizzybubbly/controllers/SearchController.java
@@ -4,6 +4,7 @@ import com.fizzybubbly.fizzybubbly.models.Drink;
 import com.fizzybubbly.fizzybubbly.models.DrinkData;
 import com.fizzybubbly.fizzybubbly.models.data.DrinkRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,9 +28,9 @@ public class SearchController {
     public String displaySearchResults(Model model, @RequestParam String searchTerm){
         Iterable<Drink> drinks;
         if (searchTerm.toLowerCase().equals("all") || searchTerm.equals("")){
-            drinks = drinkRepository.findAll();
+            drinks = drinkRepository.findAll(Sort.by("brand").ascending().and(Sort.by("flavor").ascending()));
         } else {
-            drinks = DrinkData.findByValue(searchTerm, drinkRepository.findAll());
+            drinks = DrinkData.findByValue(searchTerm, drinkRepository.findAll(Sort.by("brand").ascending().and(Sort.by("flavor").ascending())));
         }
         model.addAttribute("title", "find a seltz: " + searchTerm);
         model.addAttribute("drinks", drinks);
--- src/main/java/com/fizzybubbly/fizzybubbly/models/data/DrinkRepository.java
+++ src/main/java/com/fizzybubbly/fizzybubbly/models/data/DrinkRepository.java
@@ -1,12 +1,12 @@
 package com.fizzybubbly.fizzybubbly.models.data;
 
 import com.fizzybubbly.fizzybubbly.models.Drink;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 
 @Repository
 //@Transactional
-public interface DrinkRepository extends CrudRepository<Drink, Integer> {
+public interface DrinkRepository extends PagingAndSortingRepository<Drink, Integer> {
 }
