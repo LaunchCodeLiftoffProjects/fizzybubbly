1a9ec7dfc92bc5eeee4b3de08cb77bf3b378ab96
Mfurbee77 <mchristian0428@gmail.com>
2021-08-22 19:19:55 -0500

    Added class drink with 4 properties

--- src/main/java/com/fizzybubbly/fizzybubbly/controllers/ListController.java
+++ src/main/java/com/fizzybubbly/fizzybubbly/controllers/ListController.java
@@ -22,6 +22,16 @@ public class ListController {
         model.addAttribute("drinks", drinkRepository.findAll());
         return "list";
     }
+}
 
+// ToDo: Refactor Drink class into separate file, change to public
+// ToDo: Add repository class to map the csv file to the drink object
 
+class Drink {
+    public String name;
+    public String carbonation;
+    // Talk to group about considering an enum for carbonation to limit the values of that property.
+    public String flavor;
+    public double rating;
+    // Talk to group about whether we want to use a double or an int
 }
