6c494a2f95e592a944ee1e410dd693bddee62b57
Will Staton <will.staton@gmail.com>
2021-08-23 22:47:01 -0400

    added about page (needs content)

---
+++ src/main/java/com/fizzybubbly/fizzybubbly/controllers/AboutController.java
@@ -0,0 +1,18 @@
+package com.fizzybubbly.fizzybubbly.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("about")
+public class AboutController {
+
+    @GetMapping("")
+    public String about(Model model) {
+        model.addAttribute("title", "about");
+        return "about.html";
+    }
+
+}
