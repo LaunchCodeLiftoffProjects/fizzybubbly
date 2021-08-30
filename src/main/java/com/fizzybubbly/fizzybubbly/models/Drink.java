6adf25d7fc66dc12ead6f76cf929904375a10d75
elizabethtarantola <liztarantola@gmail.com>
2021-08-29 20:02:15 -0500

    images added

--- src/main/java/com/fizzybubbly/fizzybubbly/models/Drink.java
+++ src/main/java/com/fizzybubbly/fizzybubbly/models/Drink.java
@@ -15,7 +15,7 @@ public class Drink extends AbstractEntity {
 
     private int rating;
 
-    private String imagePath;
+    public String imagePath;
 
     public Drink(Brand brand, String flavor, String carbonation, int rating, String imagePath) {
         this.brand = brand;
@@ -25,7 +25,8 @@ public class Drink extends AbstractEntity {
         this.imagePath = imagePath;
     }
 
-    public Drink() {}
+    public Drink() {
+    }
 
     @Override
     public String toString() {
@@ -68,7 +69,13 @@ public class Drink extends AbstractEntity {
         return imagePath;
     }
 
+<<<<<<< HEAD
     public void setImagePath(String imagePath) {
         this.imagePath = imagePath;
     }
+=======
+//    public void setImagePath(String imagePath) {
+//        this.imagePath = imagePath;
+//    }
+>>>>>>> Liz_branch
 }
