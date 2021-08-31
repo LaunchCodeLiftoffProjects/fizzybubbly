2180e1af844d5e15c802406441d5c3c8baae4f4a
zabalav <vincent_zabala@yahoo.com>
2021-08-30 21:53:38 -0500

    displayed one seltzer details

--- src/main/java/com/fizzybubbly/fizzybubbly/controllers/ProductPageController.java
+++ src/main/java/com/fizzybubbly/fizzybubbly/controllers/ProductPageController.java
@@ -19,12 +19,6 @@ public class ProductPageController {
     private DrinkRepository drinkRepository;
 
     @GetMapping
-    public String displayDrinkList(Model model) {
-        model.addAttribute("drinks", drinkRepository.findAll());
-        return "productPage";
-    }
-
-    @GetMapping (params = {"drinkId"})
     public String displayDrinkDetails(@RequestParam(required = false) Integer drinkId, Model model) {
         Optional<Drink> result = drinkRepository.findById(drinkId);
 
@@ -32,7 +26,7 @@ public class ProductPageController {
         model.addAttribute("title", drink.toString());
         model.addAttribute("drink", drink);
 
-        return "productPage?id=${drinkId}";
+        return "productPage";
 
     }
 }
