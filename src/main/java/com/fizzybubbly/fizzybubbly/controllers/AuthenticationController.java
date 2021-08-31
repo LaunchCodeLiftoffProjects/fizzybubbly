9c8b354ba43b70392155a57154868aedef533754
elizabethtarantola <liztarantola@gmail.com>
2021-08-31 18:07:04 -0500

    registration form working

--- src/main/java/com/fizzybubbly/fizzybubbly/controllers/AuthenticationController.java
+++ src/main/java/com/fizzybubbly/fizzybubbly/controllers/AuthenticationController.java
@@ -2,11 +2,21 @@ package com.fizzybubbly.fizzybubbly.controllers;
 
 import com.fizzybubbly.fizzybubbly.models.User;
 import com.fizzybubbly.fizzybubbly.models.data.UserRepository;
+import com.fizzybubbly.fizzybubbly.models.dto.RegisterFormDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 import java.util.Optional;
 
+@Controller
 public class AuthenticationController {
 
     @Autowired
@@ -33,10 +43,44 @@ public class AuthenticationController {
         session.setAttribute(userSessionKey, user.getId());
     }
 
-//    @GetMapping("/signup")
-//    public String displayRegistrationForm(Model model) {
-//        model.addAttribute(new RegisterFormDTO());
-//        model.addAttribute("title", "Register");
-//        return "register";
-//    }
+    @GetMapping("/register")
+    public String displayRegistrationForm(Model model) {
+        model.addAttribute(new RegisterFormDTO());
+        model.addAttribute("title", "Register");
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String processRegistrationForm(@ModelAttribute @Valid RegisterFormDTO registerFormDTO,
+                                          Errors errors, HttpServletRequest request,
+                                          Model model) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Register");
+            return "register";
+        }
+
+        User existingUser = userRepository.findByUsername(registerFormDTO.getUsername());
+
+        if (existingUser != null) {
+            errors.rejectValue("username", "username.alreadyexists", "A user with that username already exists");
+            model.addAttribute("title", "Register");
+            return "register";
+        }
+
+        String password = registerFormDTO.getPassword();
+        String verifyPassword = registerFormDTO.getVerifyPassword();
+        if (!password.equals(verifyPassword)) {
+            errors.rejectValue("password", "passwords.mismatch", "Passwords do not match");
+            model.addAttribute("title", "Register");
+            return "register";
+        }
+
+        User newUser = new User(registerFormDTO.getUsername(), registerFormDTO.getPassword());
+        userRepository.save(newUser);
+        setUserInSession(request.getSession(), newUser);
+
+        return "redirect:";
+    }
 }
+
--- src/main/java/com/fizzybubbly/fizzybubbly/models/User.java
+++ src/main/java/com/fizzybubbly/fizzybubbly/models/User.java
@@ -10,15 +10,6 @@ import javax.validation.constraints.NotNull;
 public class User extends AbstractEntity {
 
     @NotNull
-    private String firstName;
-
-    @NotNull
-    private String lastName;
-
-    @NotNull
-    private String email;
-
-    @NotNull
     private String username;
 
     @NotNull
@@ -26,36 +17,11 @@ public class User extends AbstractEntity {
 
     private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
-    public User(String firstName, String lastName, String email, String username, String password) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
+    public User() {}
+
+    public User(String username, String password) {
         this.username = username;
         this.pwHash = encoder.encode(password);
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public String getUsername() {
