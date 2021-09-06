f27ab759c19059f74f772e0179c26d61224cc7ca
Will Staton <will.staton@gmail.com>
2021-09-06 19:42:46 -0400

    got user_id pulled from session figured out

--- src/main/java/com/fizzybubbly/fizzybubbly/controllers/ProfileController.java
+++ src/main/java/com/fizzybubbly/fizzybubbly/controllers/ProfileController.java
@@ -1,18 +1,26 @@
 package com.fizzybubbly.fizzybubbly.controllers;
 
+import com.fizzybubbly.fizzybubbly.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequestMapping("profile")
 public class ProfileController {
 
+    @Autowired
+    AuthenticationController authenticationController;
+
     @GetMapping
-    public String profile(Model model) {
+    public String profile(Model model, HttpSession session) {
         model.addAttribute("title", "your profile");
-        System.out.println("this works");
+        User user = authenticationController.getUserFromSession(session);
+        System.out.println(user.getEmail());
         return "profile";
     }
 
