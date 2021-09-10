d8b954113135bb47e80b0fa8667820852f83088b
Will Staton <will.staton@gmail.com>
2021-09-10 14:45:13 -0500

    changed RequestParam to PathVariable in DrinkController

--- src/main/java/com/fizzybubbly/fizzybubbly/controllers/DrinkController.java
+++ src/main/java/com/fizzybubbly/fizzybubbly/controllers/DrinkController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -18,8 +19,8 @@ public class DrinkController {
     @Autowired
     private DrinkRepository drinkRepository;
 
-    @GetMapping
-    public String displayDrinkDetails(@RequestParam(required = false) Integer id, Model model) {
+    @GetMapping("/{id}")
+    public String displayDrinkDetails(@PathVariable Integer id, Model model) {
         Optional<Drink> result = drinkRepository.findById(id);
 
         Drink drink = result.get();
