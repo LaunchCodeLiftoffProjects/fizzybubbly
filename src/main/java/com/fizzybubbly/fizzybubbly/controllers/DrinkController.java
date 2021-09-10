e4a4c4ae6c8c2a8033931c4b950118d8f6e024f6
Will Staton <will.staton@gmail.com>
2021-09-10 15:43:32 -0500

    refactored review class property reviewText for clarity

--- src/main/java/com/fizzybubbly/fizzybubbly/controllers/DrinkController.java
+++ src/main/java/com/fizzybubbly/fizzybubbly/controllers/DrinkController.java
@@ -5,10 +5,7 @@ import com.fizzybubbly.fizzybubbly.models.data.DrinkRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -19,15 +16,47 @@ public class DrinkController {
     @Autowired
     private DrinkRepository drinkRepository;
 
+//    @GetMapping("/{id}")
+//    public String displayDrinkDetails(@PathVariable Integer id, Model model) {
+//        Optional<Drink> result = drinkRepository.findById(id);
+//        Drink drink = result.get();
+//
+//        model.addAttribute("title", drink.toString());
+//        model.addAttribute("drink", drink);
+//
+//        return "drink";
+//
+//    }
+//
+//    @PostMapping("/{id}")
+//    public String submitNewReview(@PathVariable Integer id, Model model) {
+//
+//        return "drink";
+//    }
+
+
+//````````````TEST AREA````````````````
+
     @GetMapping("/{id}")
     public String displayDrinkDetails(@PathVariable Integer id, Model model) {
         Optional<Drink> result = drinkRepository.findById(id);
-
         Drink drink = result.get();
+
         model.addAttribute("title", drink.toString());
         model.addAttribute("drink", drink);
 
         return "drink";
 
+    }
+
+    @PostMapping("/{id}")
+    public String submitNewReview(@PathVariable Integer id, Model model) {
+        Optional<Drink> result = drinkRepository.findById(id);
+        Drink drink = result.get();
+
+        model.addAttribute("title", drink.toString());
+        model.addAttribute("drink", drink);
+
+        return "drink";
     }
 }
