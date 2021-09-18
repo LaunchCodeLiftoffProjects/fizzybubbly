7051de39110eb8b1e2c387eb066285c173c71765
James Margherio <jimargherio@me.com>
2021-09-18 18:49:14 -0500

    All filters (carbonation, brand, rating) work independently and together. search.html reworked to improve layout.

--- src/main/java/com/fizzybubbly/fizzybubbly/models/DrinkData.java
+++ src/main/java/com/fizzybubbly/fizzybubbly/models/DrinkData.java
@@ -69,45 +69,4 @@ public class DrinkData {
 
     }
 
-//    public static ArrayList<Drink> findByFieldAndValue(String field, String value, Iterable<Drink> allDrinks) {
-//
-//        String lower_val = value.toLowerCase();
-//
-//        ArrayList<Drink> filter = new ArrayList<>();
-//        ArrayList<Drink> results = new ArrayList<>();
-//
-//        if (lower_val.equals("all")){
-//            return (ArrayList<Drink>) allDrinks;
-//        }
-//
-//        if (field.equals("all")){
-//            results = findByValue(value, allDrinks);
-//            return results;
-//        }
-//
-//        for (Drink drink : allDrinks) {
-//
-//            if (drink.getBrand().toLowerCase().contains(field)) {
-//                filter.add(drink);
-//            }
-//            if (drink.getCarbonation().toLowerCase().contains(field)) {
-//                filter.add(drink);
-//            }
-//        }
-//
-//        if (value.equals("")) {
-//            return filter;
-//        } else {
-//            results = findByValue(value, filter);
-//            return results;
-//        }
-//
-//    }
-
-
-
-//    else {
-//        filter = findByValue(value, allDrinks);
-//    }
-
 }
